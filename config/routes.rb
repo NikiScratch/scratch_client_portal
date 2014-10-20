9fc9e24b027df979a2662fbabff3ebff6a04ea17
Nicole <nicole@scratch.com>
2014-10-20 13:44:02 -0400

    Making changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 ScratchClientPortal::Application.routes.draw do
 
   get 'eventvenue/index'
-
   get 'eventvenue/show'
 
 root to: 'sessions#new'
   match '/venues', to: 'eventvenue#index', via: [:get, :post]
+  match '/events', to: 'eventsuser#index', via: [:get, :post]
   match '/venue/events', to: 'eventvenue#show', via: [:get, :post]
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
